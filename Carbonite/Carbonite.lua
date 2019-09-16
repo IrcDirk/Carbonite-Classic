0e4c45f6b8c58504ba7dda37dec043ff84a3cdba
IrcDirk <ircdirk@gmail.com>
2019-09-16 20:00:54 +0200

    Changing Carbonite Classic version to 113 (as WoW 1.13)

--- Carbonite/Carbonite.lua
+++ Carbonite/Carbonite.lua
@@ -25,7 +25,7 @@ local L = LibStub("AceLocale-3.0"):GetLocale("Carbonite")
 Nx.WebSite = "wowinterface.com"
 NXTITLEFULL = L["Carbonite"]
 
-Nx.VERMAJOR			= 8.0
+Nx.VERMAJOR			= 113
 Nx.VERMINOR			= .0				-- Not 0 is a test version
 Nx.BUILD			= "436eed1"
 if Nx.BUILD:find("Format:%h", 1, true) then Nx.BUILD = string.sub("@project-revision@", 0, 7) end

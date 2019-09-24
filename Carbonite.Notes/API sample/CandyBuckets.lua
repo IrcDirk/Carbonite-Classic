de1c53b250b0729184870f6f59e7feaef5afcca0
IrcDirk <ircdirk@gmail.com>
2019-09-24 10:57:00 +0200

    Adding Notes module.

---
+++ Carbonite.Notes/API sample/CandyBuckets.lua
@@ -0,0 +1,5 @@
+﻿Nx.Notes:AddonNote("Candy Buckets - Alliance","Refuge Point",4,16,40,49)
+Nx.Notes:AddonNote("Candy Buckets - Alliance","Dragon's Mouth",4,17,21,56)
+Nx.Notes:AddonNote("Candy Buckets - Alliance","Fuselight",4,17,66,36)
+Nx.Notes:AddonNote("Candy Buckets - Alliance","Surwich",4,19,45,88)
+Nx.Notes:AddonNote("Candy Buckets - Alliance","Nethergarde Keep",4,19,61,14)

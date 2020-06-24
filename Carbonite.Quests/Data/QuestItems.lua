77f11d2a906a48729e83ce883d76ab8be8f69380
TULOA <ajgravlin_master@yahoo.com>
2020-06-24 12:56:22 -0500

    Add files via upload

--- Carbonite.Quests/Data/QuestItems.lua
+++ Carbonite.Quests/Data/QuestItems.lua
@@ -1,6 +1,6 @@
 Nx.QItems = {}  --Array to hold list of items used in quests.
 -- Format: [1]: ItemLink. Can be from wowhead. [2]: ItemID. Can be copied from ItemLink
 --TODO: Add [3]: Charges. Can get this from wowhead. Might consider a function from in game as well.
-Nx.QItems[4763] = {[1] = "\124cffffffff\124Hitem:12346::::::::60:::::\124h[Empty Cleansing Bowl]\124h\124r", [2] = 12346}
-Nx.QItems[5321] = {[1] = "\124cffffffff\124Hitem:13536::::::::60:::::\124h[Horn of Awakening]\124h\124r", [2] = 13536}
-Nx.QItems[1016] = {[1] = "\124cffffffff\124Hitem:5456::::::::60:::::\124h[Divining Scroll]\124h\124r", [2] = 5456}
+Nx.QItems[4763] = 12346
+Nx.QItems[5321] = 13536
+Nx.QItems[1016] = 5456

e0156aecc1522b246d287a5aef692bb976b203dd
Albert Valiev <artscout@gmail.com>
2025-06-06 15:56:21 +0200

    locale fix again

--- Carbonite.Notes/NxFav.lua
+++ Carbonite.Notes/NxFav.lua
@@ -126,7 +126,7 @@ local function notesConfig()
 					type = "toggle",
 					width = "full",
 					name = L["Display RareScanner icons On Map"],
-					desc = L["If you have RareScanner installed, allows it's icons on the Carbonite map"],
+					desc = L["If you have RareScanner installed, allows its icons on the Carbonite map"],
 					get = function()
 						return Nx.fdb.profile.Notes.RareScanner
 					end,
